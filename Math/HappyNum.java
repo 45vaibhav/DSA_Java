71c58225d93604b791b58b3806e4ae4a304c0acb
Vaibhav <vaibhavkapde445@gmail.com>
2025-12-15 20:14:34 +0530

    practice day 5 - 4

---
+++ Math/HappyNum.java
@@ -0,0 +1,47 @@
+// check number is happy or not 
+
+import java.util.*;
+
+class HappyNum{
+	public void happy(int n){
+		int num=n;
+		if(n==1){
+		System.out.println("Happy number :"+num);
+		}
+		if(n<=0){
+		System.out.println("is not Happy number :"+num);
+		}
+		int rem=0;
+		int temp=0;
+		int total=0;
+		while(n!=1 && n!=4){
+		total=0;
+		
+		while(n>0){
+		rem=n%10;
+		temp=rem*rem;
+		total=total+temp;
+		n=n/10;
+		   }	
+		n=total;
+		}
+		if(n==1){
+		System.out.println("Happy number :"+num);
+		}else{
+		System.out.println("is not Happy number :"+num);
+		}
+	}
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number :");
+		int n=sc.nextInt();
+		HappyNum h=new HappyNum();
+			h.happy(n);
+	}
+}
+
+/*
+Enter number :
+32
+Happy number :32
+*/
