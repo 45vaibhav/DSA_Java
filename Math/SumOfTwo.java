eaa4567647f27e3fd50cd17a017d74fc3e02f52d
Vaibhav <vaibhavkapde445@gmail.com>
2025-12-18 00:03:40 +0530

    practice day 7-2

--- Math/SumOfTwo.java
+++ Math/SumOfTwo.java
@@ -10,6 +10,15 @@ class SumOfTwo{
 		
 		int sum=0;
 		sum=a+b;
-		System.out.println(sum);
+		System.out.println("the sum of two number is : "+sum);
 	}
 }
+
+
+/*
+Enter the first num :
+4
+Enter the first num :
+2
+the sum of two number is : 6
+*/
