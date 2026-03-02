295b24a9732c8df642f59a7f57c020c890710d1e
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-02 22:36:04 +0530

    Finding the factors of number

---
+++ Math/Factors.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Factors{
+		public static void main(String args[]){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter the number :");
+			int n=sc.nextInt();
+			double length=Math.sqrt(n);
+			ArrayList<Integer>ls=new ArrayList<>();
+			for(int i=1;i<=length;i++){
+				if(n%i==0){
+				int d=n/i;
+				ls.add(i);
+				ls.add(d);
+				}
+			
+			}
+		System.out.println(ls);
+		}
+	}
+
+
+/*
+enter the number :
+36
+[1, 36, 2, 18, 3, 12, 4, 9, 6, 6]
+*/
